b7a5e7f3d4ccba455e4e10ee0d8a3c3a7a73d412
Simone Carletti <weppos@weppos.net>
2016-02-14 01:11:15 +0100

    Remove Ruby linter
    The Ruby linter has been replaced with a linter binary written in Go.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/reporters'
-require_relative 'linter'
 
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(:color => true)
 
