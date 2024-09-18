1183ab641c2c7a7435d64a612e9059591de2fe3f
David Anderson <dave@natulte.net>
2024-09-17 22:38:27 -0700

    internal/github: implement a local cache for old PR information
    Checking the entire PSL requires looking up ~600 PRs, most of which have
    been merged and closed long ago. This is quite slow and eats into the
    limited Github API quota. Empirically, with a warm cache, this speeds up
    full PSL checks from ~15min to ~1min.

--- tools/internal/github/pr.go
+++ tools/internal/github/pr.go
@@ -3,12 +3,17 @@
 package github
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/google/go-github/v63/github"
+	"github.com/natefinch/atomic"
 )
 
 // Client is a GitHub API client that performs PSL-specific
@@ -52,6 +57,12 @@ func (c *Client) apiClient() *github.Client {
 // given pull request. Returns the PSL file for the target branch, and
 // the same but with the PR's changes applied.
 func (c *Client) PSLForPullRequest(ctx context.Context, prNum int) (withoutPR, withPR []byte, err error) {
+	var cacheHit bool
+	withoutPR, withPR, cacheHit = getCachedPRData(prNum)
+	if cacheHit {
+		return withoutPR, withPR, nil
+	}
+
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
@@ -66,7 +77,7 @@ func (c *Client) PSLForPullRequest(ctx context.Context, prNum int) (withoutPR, w
 	}
 	commitInfo, _, err := c.apiClient().Git.GetCommit(ctx, c.owner(), c.repo(), mergeCommit)
 	if err != nil {
-		return nil, nil, fmt.Errorf("getting info for trial merge SHA %q: %w", mergeCommit, err)
+		return nil, nil, fmt.Errorf("getting info for merge SHA %q: %w", mergeCommit, err)
 	}
 
 	var beforeMergeCommit string
@@ -74,10 +85,10 @@ func (c *Client) PSLForPullRequest(ctx context.Context, prNum int) (withoutPR, w
 		// PR was merged, PSL policy is to use squash-and-merge, so
 		// the pre-PR commit is simply the parent of the merge commit.
 		beforeMergeCommit = commitInfo.Parents[0].GetSHA()
-	} else if !pr.GetMergeable() {
+	} else if !pr.GetMerged() && !pr.GetMergeable() {
 		// PR isn't merged, and there's a merge conflict that prevents
 		// us from knowing what the pre- and post-merge states are.
-		return nil, nil, fmt.Errorf("cannot get PSL for PR %d, needs rebase", prNum)
+		return nil, nil, fmt.Errorf("cannot get PSL diff for PR %d, needs rebase", prNum)
 	} else {
 		// PR is open, which means the merge commit is a "trial merge"
 		// that shows what would happen if you merged the PR. The
@@ -106,6 +117,11 @@ func (c *Client) PSLForPullRequest(ctx context.Context, prNum int) (withoutPR, w
 	if err != nil {
 		return nil, nil, err
 	}
+	if pr.GetMerged() {
+		// Only cache PRs that were already merged, in-progress PRs
+		// might change.
+		putCachedPRData(prNum, withoutPR, withPR)
+	}
 	return withoutPR, withPR, nil
 }
 
@@ -126,4 +142,67 @@ func (c *Client) PSLForHash(ctx context.Context, hash string) ([]byte, error) {
 		return nil, err
 	}
 	return []byte(ret), nil
+}
+
+type prCacheEntry struct {
+	WithoutPR, WithPR []byte
+}
+
+func getCachedPRData(prNum int) (withoutPR, withPR []byte, ok bool) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		// No usable cache dir, can't cache anything.
+		return nil, nil, false
+	}
+
+	cachePath := filepath.Join(cacheDir, "psltool/merged-pr", fmt.Sprintf("%d.json.gz", prNum))
+	bs, err := os.ReadFile(cachePath)
+	if err != nil {
+		return nil, nil, false
+	}
+
+	gr, err := gzip.NewReader(bytes.NewReader(bs))
+	if err != nil {
+		return nil, nil, false
+	}
+
+	var ent prCacheEntry
+	if err := json.NewDecoder(gr).Decode(&ent); err != nil {
+		return nil, nil, false
+	}
+
+	return ent.WithoutPR, ent.WithPR, true
+}
+
+func putCachedPRData(prNum int, withoutPR, withPR []byte) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		// No usable cache dir, can't cache anything.
+		return
+	}
+	cachePath := filepath.Join(cacheDir, "psltool/merged-pr", fmt.Sprintf("%d.json.gz", prNum))
+	if _, err := os.Stat(cachePath); err == nil {
+		// Already cached
+		return
+	}
+
+	if err := os.MkdirAll(filepath.Dir(cachePath), 0700); err != nil {
+		return
+	}
+
+	ent := prCacheEntry{
+		WithoutPR: withoutPR,
+		WithPR:    withPR,
+	}
+
+	var bs bytes.Buffer
+	gw := gzip.NewWriter(&bs)
+	if err := json.NewEncoder(gw).Encode(ent); err != nil {
+		return
+	}
+	if err := gw.Close(); err != nil {
+		return
+	}
+
+	atomic.WriteFile(cachePath, &bs)
 }
